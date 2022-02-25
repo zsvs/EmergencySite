19bf0028302096a6bdf7e744075710ba15d9e66a
SVS <v.stepanets@kernel.ua>
2022-02-25 23:31:55 +0200

    Emergency site for important info

---
+++ Lesson1-hw/output.tf
@@ -0,0 +1,22 @@
+﻿output "data_aws_AZ" {
+  value = data.aws_availability_zones.available.names
+}
+
+output "Elastic_IP" {
+  value = aws_eip.instance_static_addr.address
+}
+output "data_vpc_ids" {
+  value = data.aws_vpc.default.id
+}
+
+output "data_aws_region_id" {
+  value = data.aws_region.current.id
+}
+
+output "data_aws_ec2_instance_name" {
+  value = aws_instance.web_server.tags["Name"]
+}
+
+output "data_aws_ec2_instance_id" {
+  value = aws_instance.web_server.id
+}
