ac5d229d2b50cd7dceca77e2196dd88180e7d3ff
SVS <v.stepanets@kernel.ua>
2022-03-14 11:47:25 +0200

    Add KEEP-ALIVE header

--- Requster.py
+++ Requster.py
@@ -12,7 +12,8 @@ class HTTPClient:
                             "Content-type": "application/json",
                             "Accept": "*/*",
                             "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
-                            "Content-Encoding": "utf-8"
+                            "Content-Encoding": "utf-8",
+                            "Keep-Alive": "timeout=10, max=100"
                         }
         self.__URL = URL
     def SetPayload(self, Data):
