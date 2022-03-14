0af7cf92a40c453db9b2b99dbeb2f17c34626292
SVS <v.stepanets@kernel.ua>
2022-03-14 11:12:07 +0200

    Added GET request

--- Requster.py
+++ Requster.py
@@ -7,14 +7,14 @@ class HTTPClient:
     __URL = None
     __Answer = None
 
-    def __init__(self):
+    def __init__(self, URL):
         self.__Headers = {
                             "Content-type": "application/json",
                             "Accept": "*/*",
                             "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
                             "Content-Encoding": "utf-8"
                         }
-
+        self.__URL = URL
     def SetPayload(self, Data):
         """
         Setting up HTTP payload.
@@ -34,6 +34,17 @@ class HTTPClient:
         """
         if self.__Payload:
             self.__Answer = requests.post(self.__URL, data = self.__Payload, headers = self.__Headers)
+            print(self.GetAnswer())
+        else:
+            raise Exception("Payload must be setted")
+
+    def GetRequest(self):
+        """
+        Sending GET request to web-server
+        """
+        if self.__Payload:
+            self.__Answer = requests.get(self.__URL, data = self.__Payload, headers = self.__Headers)
+            print(self.GetAnswer())
         else:
             raise Exception("Payload must be setted")
 
